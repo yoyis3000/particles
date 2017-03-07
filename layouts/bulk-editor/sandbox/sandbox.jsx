9a24b76d94be18f1d1272735f11988aa229be9b2
Christian Rivera <chris.arevir@gmail.com>
2017-03-06 20:56:36 -0800

    Start of Bulk Editor

---
+++ layouts/bulk-editor/sandbox/sandbox.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import BulkEditor from '../src';
+
+render(
+  <BulkEditor />,
+  window.document.getElementById('root'),
+);
---
+++ layouts/bulk-editor/src/BulkEditor.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import cx from 'classnames';
+// import baseStyles from './BulkEditor.scss';
+// import defaultStyles from './BulkEditorDefault.scss';
+// import composeStyles from '../../../shared/stylesheetComposer';
+
+const BulkEditor = () => <div />;
+
+BulkEditor.propTypes = {};
+
+BulkEditor.defaultProps = {};
+
+export default BulkEditor;

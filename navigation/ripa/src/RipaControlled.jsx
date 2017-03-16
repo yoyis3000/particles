26dd38e1b50934158dd2fa82c8aeea213c51cb7e
Ben Burlingham <ben.burlingham@procore.com>
2017-03-16 10:57:20 -0700

    Revert "Ripa: Removing local storage persistence." (#11)
    * Revert "Tiwae (#9)"
    
    This reverts commit 4c069df15cc18a40dd7246757053d2f006728119.
    
    * Revert "Ripa: Removing local storage persistence. (#10)"
    
    This reverts commit 8ad5b7aa555590fdaa7eb6b9d2119e31e2649d1c.

--- navigation/ripa/src/RipaControlled.jsx
+++ navigation/ripa/src/RipaControlled.jsx
@@ -2,11 +2,45 @@ import React, { PropTypes } from 'react';
 import qs from 'qs';
 import Ripa from './Ripa';
 
+const RipaControlled = ({
+  onChange,
+  scope,
+  ...props
+}) => {
+  onRipaLoad(scope);
+
+  return (
+    <Ripa
+      {...props}
+      onChange={onRipaChange(scope, onChange)}
+      selectedKey={getCurrentViewFromBrowser(scope)}
+    />
+  );
+};
+
+const onRipaLoad = (scope) => {
+  const view = localStorage.getItem(`${scope}/VIEW`);
+
+  if (view) {
+    const url = window.location.href.split('?');
+    const params = qs.parse(url[1]);
+
+    const newParams = Object.assign(params, { view });
+    history
+      .pushState(
+        history.state,
+        '',
+        `${url[0]}?${qs.stringify(newParams, { arrayFormat: 'brackets' })}`);
+  }
+};
+
 const onRipaChange = (scope, handler) => (k, v, index) => {
   const url = window.location.href.split('?');
   const params = qs.parse(url[1]);
 
   if (params.view !== k) {
+    localStorage.setItem(`${scope}/VIEW`, k);
+
     const newParams = Object.assign(params, { view: k });
     history
       .pushState(
@@ -17,30 +51,13 @@ const onRipaChange = (scope, handler) => (k, v, index) => {
   }
 };
 
-const RipaControlled = ({
-  onChange,
-  scope,
-  ...props
-}) => {
-  const getCurrentViewFromBrowser = () => {
-    const params = qs.parse(window.location.search.substr(1));
-    return params.view || props.labels[0].k;
-  };
-
-  return (<Ripa
-    {...props}
-    onChange={onRipaChange(scope, onChange)}
-    selectedKey={getCurrentViewFromBrowser(scope)}
-  />);
+const getCurrentViewFromBrowser = (scope) => {
+  const params = qs.parse(window.location.search.substr(1));
+  return params.view || localStorage.getItem(`${scope}/VIEW`);
 };
 
-RipaControlled.propTypes = {
-  ...Ripa.propTypes,
+RipaControlled.propTypes = {...Ripa.propTypes,
   scope: PropTypes.string
-};
-
-RipaControlled.defaultProps = {
-  scope: 'procore'
 };
 
 export default RipaControlled;
